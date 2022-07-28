554e58ac0e951b315ce7acf468ddf34fbdae7be8
Rodolfo Braquetes <braquetesss@gmail.com>
2022-07-28 17:23:38 -0500

    first commit

---
+++ src/database.js
@@ -0,0 +1,33 @@
+const mysql = require("mysql");
+const fs = require('fs');
+
+const mysqlConnection = mysql.createConnection({
+    // host: 'localhost',
+    // user: 'root',
+    // password: '',
+    // database: 'asesorias'
+
+  host:"asesorias.mysql.database.azure.com",
+  user:"braquetes",
+  password:"Rodo2507137946",
+  database:"dazabi",
+  port:3306,
+  ssl:{ca:fs.readFileSync("DigiCertGlobalRootCA.crt.pem")}
+
+    // host: process.env.HOST,
+    // user: process.env.MYSQL_USER,
+    // password: process.env.PASSWORD,
+    // database: process.env.DATABASE,
+    // port: 3306,
+});
+
+mysqlConnection.connect(function(err) {
+    if (err) {
+        console.error(err);
+        return;
+    } else {
+        console.log("Database is connected");
+    }
+});
+
+module.exports = mysqlConnection;
---
+++ src/index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const app = express();
+
+// Settings
+app.set('port', process.env.PORT || 3000);
+
+// Middlewares
+app.use(express.json());
+app.use(function (req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  next();
+});
+
+// Routes
+app.get("/", (req, res) => {
+    res.json({ "Message": "DAZABI" })
+});
+
+app.use("/users", require('./routes/usuarios'));
+app.use("/recompensas", require('./routes/recompensas'));
+app.use("/puntos", require('./routes/puntos'));
+app.use("/historial", require('./routes/historial'));
+
+// Starting the server
+app.listen(app.get('port'), () => {
+    console.log(`Server on port ${app.get('port')}`);
+});
